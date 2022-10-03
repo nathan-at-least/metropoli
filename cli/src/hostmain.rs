a9feb2e36a21766c68862b06e57f87b9cca6a2b4
Nate Wilcox <nathan+dev@electriccoin.co>
2022-10-03 11:25:59 -0700

    Update the public interface and API docs for `metropoli_host_cli`.

--- cli/src/hostmain.rs
+++ cli/src/hostmain.rs
@@ -1,5 +1,6 @@
 use crate::Options;
 
+/// Run the main process which parses args then executes the host
 pub fn host_main() -> anyhow::Result<()> {
     let opts = Options::parse();
     todo!("{:#?}", opts)
